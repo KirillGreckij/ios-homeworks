bf40b4ade19808b7215e69209e4ff01ff0879324
Kirill Greckij <kirill.greckij@yandex.ru>
2022-11-23 10:26:02 +0300

    Change Profile

---
+++ Netology_IB_Instruments/XIB/ProfileView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Кирилл Грецкий on 23.11.2022.
+//
+
+import UIKit
+
+class ProfileView : UIView {
+    @IBOutlet weak var profileImage: UIImageView!
+    
+    @IBOutlet weak var profileName: UILabel!
+    
+    @IBOutlet weak var profileBirthday: UILabel!
+    
+    @IBOutlet weak var profileCity: UILabel!
+    
+    @IBOutlet weak var profileContent: UITextView!
+    
+    
+    
+}

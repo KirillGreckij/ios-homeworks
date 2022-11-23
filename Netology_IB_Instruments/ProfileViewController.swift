727bb7a416f3969ee5bafce47b2cfa5bac31581a
Kirill Greckij <kirill.greckij@yandex.ru>
2022-11-23 10:42:14 +0300

    HomeWork is done

--- Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/ProfileViewController.swift
@@ -8,5 +8,17 @@
 import UIKit
 
 class ProfileViewController : UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadNib()
+    }
     
+    func loadNib() {
+        if let nibView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            nibView.frame = CGRect(x: 0, y: 150, width: UIScreen.main.bounds.width, height: 400)
+            view.addSubview(nibView)
+        } else {
+            print("Error")
+        }
+    }
 }
